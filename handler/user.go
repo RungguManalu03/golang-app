1cf40c81ffbdffaa216ce27c2b6c18fc7d34a17e
RungguManalu <runggumanalu8@gmail.com>
2023-04-23 19:54:15 +0700

    api-login

--- handler/user.go
+++ handler/user.go
@@ -47,3 +47,41 @@ func (h *userHandler) RegisterUser(c *gin.Context){
 
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	//user memasukkan input email dan password
+	//input ditangkap handler
+	//mapping dari input user ke input struct
+	//input struct dipassing kedalam service 
+	//dalam service mencari dengan bantuan repository user dengan email yang sudah dimasukkan tadi
+	//mencocokkan password
+
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Login Failed",http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+
+		response := helper.APIResponse("Login Failed",http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedinUser, "tokentoken")
+
+	response := helper.APIResponse("Succesfully loggedin", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
+
